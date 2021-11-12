63ebf5e07f8071d76ab60b1c8336f37252bc523c
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-12 15:14:51 -0500

    Add attributes test for dr show page

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'doctors show page' do
+  before :each do
+    @hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
+    @dr_bailey = hospital.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+  end
+
+  it 'shows doctor information' do
+    visit "/doctors/#{@dr_bailey.id}"
+
+    expect(page).to have_content(@dr_bailey.name)
+    expect(page).to have_content(@dr_bailey.specialty)
+    expect(page).to have_content(@dr_bailey.univeristy)
+  end
+end
